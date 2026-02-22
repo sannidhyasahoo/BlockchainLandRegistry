cb70241b16100ac377351fb79051c08bda3b863b
Shan <92211264+sannidhyasahoo@users.noreply.github.com>
2026-02-22 09:09:02 +0530

    fixing

--- land-registry-blockchain/frontend/vite.config.js
+++ land-registry-blockchain/frontend/vite.config.js
@@ -4,8 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  define: {
+    // Replace the string below with your ACTUAL raw Pinata JWT
+    // Make sure it is wrapped in double quotes inside the single quotes!
+    'import.meta.env.VITE_PINATA_JWT': '"eyJhbGciOiJIUzI1NiIsInR5cCI6Ikp..."' 
+  }
 })
